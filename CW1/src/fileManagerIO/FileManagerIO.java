0518c7e3fa64e0a2b73dee8b6c73d7ad9293280c
gordo851 <gr2@hw.ac.uk>
2019-02-07 19:14:54 +0000

    So all changes suggested in the meeting have been made. First time writing a singleton so check that careful please. The report written right now is very basic. I would see us expanding on this in iteration 2 when we know what we want to tell the user. Also my totalIncome method works out the total income without taking into account the discounts made. I'll implement this once Max has written the basket class and I know what the discounts are. We might just want to hold the total income made from each order somewhere. I dunno. Also just throwing the fileNotFound exception just now. Will deal with it properly once we start writing custom exceptions.

--- CW1/src/fileManagerIO/FileManagerIO.java
+++ CW1/src/fileManagerIO/FileManagerIO.java
@@ -1,46 +1,135 @@
 package fileManagerIO;
 
 import java.util.*;
+import java.util.Date;
 
 import order.Order;
 import order.Product;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileManagerIO {
+	//Making this a singleton class
+	private static FileManagerIO firstInstance = null;
+	private FileManagerIO() {}
+	public static FileManagerIO getInstances() {
+		if(firstInstance == null) {
+			firstInstance = new FileManagerIO();
+		}
+		return firstInstance;
+	}
+	
 	ArrayList<Order> currentOrders;
 	ArrayList<Order> existingOrders;
 	TreeSet<Product> products;
+	private int rows = 0;
 
-//reads each line of a file that's passed to it
-	public void readFromFile(String fileName) 
+	public int getSizeOfCurrentOrder() 
+	{
+		int size  = currentOrders.size();
+		return size;
+	}
+
+	public int getSizeOfExistingOrder() 
+	{
+		int size  = existingOrders.size();
+		return size;
+	}
+
+	public int getNumberOfProducts() 
+	{
+		int size  = products.size();
+		return size;
+	}
+
+	//reads each line of a file that's passed to it
+	public void readFromProductsFile(String fileName) 
 	{
 		File file = new File(fileName);
 		try {
 			Scanner scanner = new Scanner(file);	
 			while (scanner.hasNextLine()) {
 				String inputLine = scanner.nextLine();
-				processLine(inputLine);
+				processMenuLine(inputLine);
 			}
 		}
 		catch (FileNotFoundException e) {
 			System.out.print("File: " + fileName + " cannot be found.");
-		}	
+		}
 	}
-//adds products from file to an array list
-	private void addProduct(Product c) {
-		products.add(c);
+	//processes each line of the Products file.
+	private void processMenuLine(String inputLine) {
+		String part[] = inputLine.split(",");
+		String name = part[0];
+		String desc = part[1];
+		float price = Float.parseFloat(part[2]);
+		String cat = part[3];
+		String id = part[4];
+		Product thisProduct = new Product(name, desc, price, cat, id);
+		this.addProduct(thisProduct);
 	}
-//adds orders from file to existing orders list
-	private void addExistingOrder(Order a) {
-		existingOrders.add(a);
+
+	//reads each line of a file that's passed to it
+	public void readFromOrderFile(String fileName) 
+	{
+		File file = new File(fileName);
+		try {
+			Scanner scanner = new Scanner(file);	
+			while (scanner.hasNextLine()) {
+				String inputLine = scanner.nextLine();
+				processOrderLine(inputLine);
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.print("File: " + fileName + " cannot be found.");
+		}
 	}
-//adds new orders (from the GUI) to current orders
-	private void addCurrentOrder(Order a) {
-		currentOrders.add(a);
+	//processes each line of the Products file.
+	private void processOrderLine(String inputLine) {
+		String part[] = inputLine.split(",");
+		long timeStamp = Integer.parseInt(part[0]);
+		Product product = findProduct(part[2]);
+		String custID = part[1];
+		Order a = new Order(timeStamp, product, custID);
+		this.addExistingOrder(a);
+		rows = rows + 1;
+
+	}		
+
+	//adds products from file to an array list
+	private void addProduct(Product p) {
+		products.add(p);
 	}
-//checks if a string is a number - used in process line
+
+	//adds orders from file to existing orders list
+	private void addExistingOrder(Order o) {
+		existingOrders.add(o);
+	}
+
+	private String createCustomerID() {
+		Order lastOrder = existingOrders.get(existingOrders.size()-1);
+		String lastCustomerID = lastOrder.getCustID();
+		int lastCustomerNum = Integer.parseInt(lastCustomerID.substring(2));
+		int newCustomerNum = lastCustomerNum + 1;
+		String newCustomerStr = Integer.toString(newCustomerNum);
+		String newCustomerID = "CUS" + newCustomerStr;
+		return newCustomerID;
+	}
+
+	//adds new orders (from the GUI) to current orders
+	public void addCurrentOrder(Product p) throws IOException {
+		Date date = new Date();
+		long timeStamp = date.getTime();
+		String customerID = createCustomerID();
+		Order o = new Order(timeStamp, p, customerID);
+		store(o);
+		currentOrders.add(o);
+	}
+
+	//checks if a string is a number - used in process line
 	public static boolean isNumber(String c){
 		boolean isNumber = true;
 		try {
@@ -51,7 +140,8 @@ public class FileManagerIO {
 		}
 		return isNumber;
 	}
-//for creating existing orders. Finds Product obejects from product ID
+
+	//for creating existing orders. Finds Product objects from product ID
 	private Product findProduct(String productID) {
 		Product thisProduct = null;
 		for (Product a : products) {
@@ -61,24 +151,56 @@ public class FileManagerIO {
 		}
 		return thisProduct;
 	}
-//processes each line of the file.
-	private void processLine(String inputLine) {
-		String part[] = inputLine.split(",");
-		if (isNumber(part[1]) == true) {
-			int timeStamp = Integer.parseInt(part[1]);
-			Product product = findProduct(part[3]);
-			String custID = part[2];
-			Order a = new Order(timeStamp, product, custID);
-			this.addExistingOrder(a);
+	public void store(Order o) throws IOException {
+		FileWriter writer = new FileWriter("Orders.csv");
+		String timestamp = Long.toString(o.getTimestamp());
+		String customerID = o.getCustID();
+		Product product = o.getProduct();
+		String productID = product.getId();
+		writer.append(timestamp);
+		writer.append(",");
+		writer.append(customerID);
+		writer.append(",");
+		writer.append(productID);
+		writer.append("\n");
+	}
+	private int timesProductWasOrdered(Product p) {
+		int timesOrdered = 0;
+		for(Order o: currentOrders) {
+			if(o.getProduct() == p) {
+				timesOrdered = timesOrdered + 1;
+			}
 		}
-		else {
-			String name = part[1];
-			String desc = part[2];
-			float price = Float.parseFloat(part[3]);
-			String cat = part[4];
-			String id = part[5];
-			Product thisProduct = new Product(name, desc, price, cat, id);
-			this.addProduct(thisProduct);
+		for(Order o: existingOrders) {
+			if(o.getProduct() == p) {
+				timesOrdered = timesOrdered + 1;
+			}
+		}
+		return timesOrdered;
+	}
+	private double totalIncome() {
+		double totalIncome = 0;
+		for(Order o: currentOrders) {
+			Product p = o.getProduct();
+			double price = p.getPrice();
+			totalIncome = totalIncome + price;
+		}
+		for(Order o: existingOrders) {
+			Product p = o.getProduct();
+			double price = p.getPrice();
+			totalIncome = totalIncome + price;
+		}
+		
+		return totalIncome;
+	}
+	public void writeReport(String filename) throws IOException {
+		FileWriter fw = new FileWriter (filename); {
+			fw.write("These are all the products on offer:\n");
+			for (Product p: products) {
+				fw.write(p.getMenuDisplay() + " " + p.getDesc() + ". This item was ordered a total of " + timesProductWasOrdered(p) + " times.\n");
+			}
+			fw.write("The total income was: " + totalIncome() + "\n");
+			fw.close();
 		}
 	}
 }
