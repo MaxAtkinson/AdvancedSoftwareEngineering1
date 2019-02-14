25d532c1b06d8a8ed2c1c71fd4dc37ed787bc936
gordo851 <gr2@hw.ac.uk>
2019-02-14 22:36:47 +0000

    was still importing treeset

--- CW1/src/main/Program.java
+++ CW1/src/main/Program.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Set;
-import java.util.TreeSet;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
