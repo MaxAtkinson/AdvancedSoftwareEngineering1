9bf93b8da79bc2c4339705a4c5f0310c4dccc4fc
Max Atkinson <max@Maxs-MacBook-Pro-4.local>
2019-02-14 17:04:26 +0000

    update basket tests to reflect static method

--- CW1/src/testing/BasketTests.java
+++ CW1/src/testing/BasketTests.java
@@ -22,32 +22,25 @@ public class BasketTests {
 
 	@Test
 	public void testCalculateTotalPricePass() {
-		@SuppressWarnings("serial")
 		ArrayList<Product> products = new ArrayList<Product>() {{
 			 add(new Food("Test Food 1", "A tasty bit of testing", 2.10f, "", "FOOD123"));
 			 add(new Drink("Test Drink 1", "A tasty bit of testing", 1.10f, "", "DRINK123"));
 			 add(new Memoribilia("Test Memoribilia 1", "A tasty bit of testing", 1.00f, "", "MEM123"));
 		 }};
-		 basket = new Basket(products);
+		 basket = new Basket();
 		 
-		 assertEquals(4.20f, basket.calculateTotalPrice(), 0.00);
+		 assertEquals(4.20f, Basket.calculateTotalPrice(products), 0.00);
 	}
 
 	@Test
 	public void testCalculateTotalPriceEmptyProductsPass() {
 		 ArrayList<Product> products = new ArrayList<Product>();
-		 basket = new Basket(products);
-		 assertEquals(0.0f, basket.calculateTotalPrice(), 0.00);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testConstructorThrowsWhenNullPass() {
-		 basket = new Basket(null);
+		 basket = new Basket();
+		 assertEquals(0.0f, Basket.calculateTotalPrice(products), 0.00);
 	}
 
 	@Test
 	public void testCalculateDiscountsPass() {
-		@SuppressWarnings("serial")
 		ArrayList<Product> products = new ArrayList<Product>() {{
 			 add(new Food("Test Food 1", "A tasty bit of testing 1", 2.10f, "", "FOOD123"));
 			 add(new Food("Test Food 2", "A tasty bit of testing 2", 2.10f, "", "FOOD124"));
@@ -56,23 +49,23 @@ public class BasketTests {
 			 add(new Memoribilia("Test Memoribilia 1", "A tasty bit of testing", 1.00f, "", "MEM123"));
 			 add(new Memoribilia("Test Memoribilia 2", "A tasty bit of testing", 1.00f, "", "MEM124"));
 		}};
-		basket = new Basket(products);
-		float expected = basket.calculateTotalPrice() * 0.8f;
-		assertEquals(expected, basket.calculateDiscounts(), 0.01);
+		basket = new Basket();
+		float expected = Basket.calculateTotalPrice(products) * 0.8f;
+		assertEquals(expected, Basket.calculateDiscountedTotal(products), 0.01);
 	}
 	
 	@Test
 	public void testCalculateDiscountsDoesntGiveDiscountWhenLessThanTwoMemAndOneDrinkPass() {
-		@SuppressWarnings("serial")
 		ArrayList<Product> products = new ArrayList<Product>() {{
 			 add(new Food("Test Food 1", "A tasty bit of testing 1", 2.10f, "", "FOOD123"));
 			 add(new Food("Test Food 2", "A tasty bit of testing 2", 2.10f, "", "FOOD124"));
 			 add(new Food("Test Food 3", "A tasty bit of testing 3", 2.10f, "", "FOOD125"));
 			 add(new Memoribilia("Test Memoribilia 1", "A tasty bit of testing", 1.00f, "", "MEM123"));
 		}};
-		basket = new Basket(products);
-		float expected = basket.calculateTotalPrice();
-		assertEquals(expected, basket.calculateDiscounts(), 0.00);
+		basket = new Basket();
+
+		float expected = Basket.calculateTotalPrice(products);
+		assertEquals(expected, Basket.calculateDiscountedTotal(products), 0.00);
 	}
 	
 	@Test
@@ -80,8 +73,38 @@ public class BasketTests {
 		ArrayList<Product> products = new ArrayList<Product>() {{
 			 add(new Food("Test Food 1", "A tasty bit of testing 1", 2.10f, "", "FOOD123"));
 		}};
-		basket = new Basket(products);
+		basket = new Basket();
+	    for (Product p: products) {
+		    basket.addProduct(p);
+	    }
 		assertEquals(1, basket.getProducts().size());
+	}
+	
+	@Test
+	public void clearBasketClearsProductsListPass() {
+		ArrayList<Product> products = new ArrayList<Product>() {{
+			 add(new Food("Test Food 1", "A tasty bit of testing 1", 2.10f, "", "FOOD123"));
+		}};
+		basket = new Basket();
+	    for (Product p: products) {
+		    basket.addProduct(p);
+	    }
+	    basket.clearBasket();
+		assertEquals(0, basket.getProducts().size());
+	}
+	
+	@Test
+	public void removeProductRemovesProductsFromListPass() {
+		Product product = new Food("Test Food 1", "A tasty bit of testing 1", 2.10f, "", "FOOD123");
+		ArrayList<Product> products = new ArrayList<Product>() {{
+			 add(product);
+		}};
+		basket = new Basket();
+	    for (Product p: products) {
+		    basket.addProduct(p);
+	    }
+	    basket.removeProduct(product);
+		assertEquals(0, basket.getProducts().size());
 	}
 	
 
